4a941bbca1d8482495a63f0dae7a3f9f8dfd892f
Akira Laine <akira_laine@outlook.com>
2016-05-22 16:08:35 +1100

    added component and other updates

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-var todos = [{text: "Learn VueJS"}]
+var initialData = [{task: "Learn VueJS", completed: false}]
 
 if(window.localStorage){
-	localStorage.setItem("todos", localStorage.getItem("todos") || JSON.stringify(todos));
+	localStorage.setItem("todos", localStorage.getItem("todos") || JSON.stringify(initialData));
 } else {
-	alert("Your browser does not support LocalStorage. Todo's will not be saved")
+	document.getElementByID("app").append("Your browser does not support LocalStorage. Todo's will not be saved")
 }
 
+Vue.component("todos", {
+	template: "#app-template",
+	data: function() {
+		return {
+			value: "",
+			todos: JSON.parse(localStorage.getItem("todos"))
+		}
+	},
+	methods: {
+			addTodo: function(){
+					var item = this.value;
+					if(item.trim() !== ""){
+							this.todos.push({task: item, completed: false});
+							localStorage.setItem("todos", JSON.stringify(this.todos));
+							this.value = ""
+					}
+			},
+			removeTodo: function($index){
+					this.todos.splice($index, 1);
+					localStorage.setItem("todos", JSON.stringify(this.todos));
+			},
+			toggleStatus: function(todo){
+				todo.completed = !todo.completed;
+				localStorage.setItem("todos", JSON.stringify(this.todos))
+			}
+	}
+})
+
 new Vue({
-    el: "#app",
-    data: {
-        value: "",
-        todos: JSON.parse(localStorage.getItem("todos"))
-    },
-    methods: {
-        addTodo: function(){
-            var item = this.value;
-            if(item.trim() !== ""){
-                this.todos.push({text: item});
-                localStorage.setItem("todos", JSON.stringify(this.todos));
-                this.value = ""
-            }
-        },
-        removeTodo: function($index){
-            this.todos.splice($index, 1);
-            localStorage.setItem("todos", JSON.stringify(this.todos));
-        }
-    }
+    el: "#app"
 })
